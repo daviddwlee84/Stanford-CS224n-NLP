05c598b5913781ee6810a2c81065f1947b2fc0ab
David Lee <daviddwlee84@gmail.com>
2019-07-25 13:22:47 +0800

    Assignment 2 (b) SGD

--- Assignments/a2/code/sgd.py
+++ Assignments/a2/code/sgd.py
@@ -86,7 +86,9 @@ def sgd(f, x0, step, iterations, postprocessing=None, useSaved=False,
 
         loss = None
         ### YOUR CODE HERE
-
+        loss, gradient = f(x)
+        # update the x with gradient
+        x = x - step * gradient
         ### END YOUR CODE
 
         x = postprocessing(x)
